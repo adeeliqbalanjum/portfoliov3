4ba59bc4a198b20d11c144677f1f9444b6615f3c
adeeliqbalanjum <bitm-f20-090@superior.edu.pk>
2026-06-17 13:42:00 +0500

    Configure Pages path

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: isGithubActions ? '/portfolio-v4' : '',
-  assetPrefix: isGithubActions ? '/portfolio-v4/' : '',
+  basePath: isGithubActions ? '/portfoliov3' : '',
+  assetPrefix: isGithubActions ? '/portfoliov3/' : '',
 };
 
 export default nextConfig;
